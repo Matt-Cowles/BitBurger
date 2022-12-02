6c901cfac189c8eb33e3547478291a7fded5a56a
Matt Cowles <selwocm@gmail.com>
2022-12-02 15:36:57 -0800

    add mongoose db called bit-burger and create server port

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
+const ejs = require("ejs");
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/bit-burger");
+}
+main().then(() => console.log("Connected to db"));
+main().catch((err) => console.log("AN ERROR!", err));
+
+app.listen(3000, () => console.log("listening on port 3000"));
