9e76c84a0bd05b91a89b0e735d9261b56c238d99
Matt Cowles <selwocm@gmail.com>
2022-12-09 12:33:43 -0800

    change cart model from covoluted function to a Class

--- models/cart.js
+++ models/cart.js
@@ -1,28 +1,56 @@
-module.exports = function Cart(oldCart) {
-  this.items = oldCart.items || {};
-  this.qty = oldCart.qty || 0;
-  this.totalPrice = oldCart.totalPrice || 0;
+let cart = null;
 
-  this.add = function (item, id) {
-    let storedItem = this.items[id];
-    if (!storedItem) {
-      storedItem = this.items[id] = {
-        item: item,
-        qty: 0,
-        price: 0,
-      };
+module.exports = class Cart {
+  static save(item) {
+    if (cart) {
+      const existingItemIndex = cart.items.findIndex((i) => i.id === item.id);
+      console.log("existingItemIndex: ", existingItemIndex);
+      if (existingItemIndex > 0) {
+        const existingItem = cart.items[existingItemIndex];
+        existingItem.qty += 1;
+        cart.totalPrice += item.price;
+      } else {
+        item.qty = 1;
+        cart.items.push(item);
+        cart.totalPrice += item.price;
+      }
+    } else {
+      cart = { items: [], totalPrice: 0 };
+      item.qty = 1;
+      cart.items.push(item);
+      cart.totalPrice = item.price;
     }
-    storedItem.qty++;
-    storedItem.price = storedItem.item.price * storedItem.qty;
-    this.qty++;
-    this.totalPrice += storedItem.price;
-  };
-
-  this.generateArray = function () {
-    const arr = [];
-    for (let id in this.items) {
-      arr.push(this.items[id]);
-    }
-    return arr;
-  };
+  }
+  static getCart() {
+    return cart;
+  }
 };
+
+// function Cart(oldCart) {
+//   this.items = oldCart.items || {};
+//   this.qty = oldCart.qty || 0;
+//   this.totalPrice = oldCart.totalPrice || 0;
+
+//   this.add = function (item, id) {
+//     let storedItem = this.items[id];
+//     if (!storedItem) {
+//       storedItem = this.items[id] = {
+//         item: item,
+//         qty: 0,
+//         price: 0,
+//       };
+//     }
+//     storedItem.qty++;
+//     storedItem.price = storedItem.item.price * storedItem.qty;
+//     this.qty++;
+//     this.totalPrice += storedItem.price;
+//   };
+
+//   this.generateArray = function () {
+//     const arr = [];
+//     for (let id in this.items) {
+//       arr.push(this.items[id]);
+//     }
+//     return arr;
+//   };
+// };
