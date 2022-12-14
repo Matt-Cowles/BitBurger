b37afaeff115da8f5f489c00815b1f72011a3c08
Matt Cowles <selwocm@gmail.com>
2022-12-14 00:25:01 -0800

    create user model with passport-local-mongoose password support

--- models/user.js
+++ models/user.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: {
     type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
+    sparse: true,
   },
 });
+
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
